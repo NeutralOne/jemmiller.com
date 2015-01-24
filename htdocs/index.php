274a95b5cc05e7b4adc2049bf7c34d231132dc28
root <root@jemmiller.com>
2015-01-23 22:50:49 -0600

    makin some changes

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,1 +1,11 @@
-I'm making changes!!!!
+<html>
+        <head>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        </head>
+
+	<body>
+	<center> Hello! <br> <br> </center>
+	</center> My name is Jeremy Miller! Welcome to JemMiller.com! </center>
+	</body>
+</html>
+~
