2bf12176d464ad5d0509eadd976c322af6e5a50b
NeutralOne <jemmiller805@gmail.com>
2015-02-05 21:25:54 -0600

    doctype and such

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,7 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Jeremy Miller</title>
   </head>
