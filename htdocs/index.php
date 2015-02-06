117230bf528ef4b5a1be16707c2079bacc4579ef
NeutralOne <jemmiller805@gmail.com>
2015-02-05 20:50:23 -0600

    no more iframe

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,14 +1,70 @@
 <html>
-        <head>
-        <link rel="stylesheet" type="text/css" href="style.css">
-        </head>
-        <title>
-        Jeremy Miller
-        </title>
-        <body>
-	<center>
-        <iframe src="https://docs.google.com/document/d/1Bj9UomSJGGce2AShEg_k1TJvCBS_xRZNVaz88P9v_mE/pub?embedded=true"
- height="950" width="900" frameBorder="0" align="middle"></iframe>
-        </center>
-	</body>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Jeremy Miller</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <h2>Jeremy M. Miller</h2>
+        <div>me@jemmiller.com</div>
+        <div>San Antonio, TX (805) 698-4806<br><br></div>
+        <div class="objective">Hard-working and skilled, passionate about cutting-edge technology, seeking employment in the technology industry to jump-start a career.<br><br></div>
+      </div>
+      <div class="section">
+        <h4>Professional Skills:</h4>
+        <ul>
+          <li>Linux Administration (especially Ubuntu and CentOS)</li>
+          <li>Administer Linux users</li>
+          <li>Configure Linux file system permissions</li>
+          <li>Install and configure LAMP and LEMP stack, Wordpress</li>
+          <li>Working knowledge of DNS</li>
+          <li>Experience with MySQL</li>
+          <li>PHP application experience</li>
+          <li>Web Development (especially PHP, CSS, and HTML5)</li>
+          <li>Managed private code using Git and GitHub</li>
+          <li>Operations tools including NewRelic and Cloud Monitoring</li>
+          <li>Diverse experience with computer hardware and software</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h4>Projects:</h4>
+        <ul>
+          <li>Created a PHP application for a video game that communicates with an API (<a href="http://league.jemmiller.com">league.JemMiller.com)</a></li>
+          <li>Experienced with NewRelic for monitoring purposes</li>
+        </ul>
+      </div>     
+      <div class="section">
+        <h4>Hobbies:</h4>
+        <ul>
+          <li>Competitive strategy gamer</li>
+          <li>Designing and coding websites</li>
+          <li>Working with computers and technology (ever since I starting writing scripts for online games when I was 13)</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h4>Strengths:</h4>
+        <ul>
+          <li>Excellent communication skills, both written and verbal</li>
+          <li>Natural problem-solver</li>
+          <li>Works well both independently and in groups</li>
+          <li>Creative and inquisitive</li>
+        </ul>
+      </div>
+      <div class="section">
+        <h4>Certifications and Experience:</h4>
+        <ul>
+          <li>CloudU</li>
+          <li>Attended multiple Open Cloud Academy workshops (mostly Network+)</li>
+          <li>Administrate and develop JemMiller.com</li>
+        </ul>
+      </div>  
+      <div class="section">
+        <h4>Work Experience:</h4>
+        <ul>
+          <li>Hookah Specialist at Gypsy Cafe (August to November 2014)</li>
+        </ul>
+      </div>
+    </div>
+  </body>
 </html>
